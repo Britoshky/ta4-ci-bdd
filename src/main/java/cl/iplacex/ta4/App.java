ee2960d5395f6215e0742edffe702444866afa6b
Héctor Brito <britoshky@gmail.com>
2026-01-01 12:22:18 -0300

    feat: agrega estructura base de proyecto Maven

---
+++ src/main/java/cl/iplacex/ta4/App.java
@@ -0,0 +1,13 @@
+package cl.iplacex.ta4;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
